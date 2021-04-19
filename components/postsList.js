8e86c7f127d995e44b15f8a6d42f546b5f9d9776
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2021-04-19 17:28:18 +0300

    Fixed posts date. Began local files cleanup and posts content implementation

--- components/postsList.js
+++ components/postsList.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import ProfileImage from "./image";
 import styles from "styles/layout.module.css";
 import utilStyles from "styles/utils.module.css";
+import moment from "moment";
 import Link from "next/link";
 import LogOut from "members/components/auth/logout/LogoutButton";
 import LogIn from "members/components/auth/login/LoginButton";
@@ -14,12 +15,6 @@ const Posts = () => {
   const { user } = useAuth();
   const { app } = useRealm();
   const [sortedPosts, setSortedPosts] = useState(null);
-
-  const getPostsArray = () => {
-    return sortedPosts
-      ? Object.keys(sortedPosts).map((key) => [Number(key), sortedPosts[key]])
-      : null;
-  };
 
   const query = gql`
     query($query: PostQueryInput!) {
@@ -75,7 +70,9 @@ const Posts = () => {
             </Link>
             <br />
             <small className={utilStyles.lightText}>
-              <Date dateString={current.info.created_at} />
+              <div>
+                {moment(current.info.created_at).format("LLLL d, yyyy")}
+              </div>
             </small>
           </li>
         ))}
